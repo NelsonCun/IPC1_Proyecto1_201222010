9f2658516e967b9707da4ca216dd976691903485
NelsonCun <nelsonemanuel7@gmail.com>
2024-02-27 18:30:28 -0600

    Inicio proyecto1

---
+++ IPC1_Proyecto1_201222010/src/ipc1_proyecto1_201222010/IPC1_Proyecto1_201222010.java
@@ -0,0 +1,13 @@
+package ipc1_proyecto1_201222010;
+
+/**
+ *
+ * @author Nelson Emanuel Cún Bálan - Carné: 201222010
+ */
+public class IPC1_Proyecto1_201222010 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
